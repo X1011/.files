5afe6bc044aaa0196a052850cf233799ab339e14
dan smith <XX1011@gmail.com>
2019-06-20 07:05:26 -0400

    twitchup.sh: fix empty argument
    this may interfere with quoted arguments, though. needs further investigation

--- bin/twitchup.sh
+++ bin/twitchup.sh
@@ -7,7 +7,7 @@ id=$2
 rest=${@:3}
 pushd ~/.tubeup/downloads/ >/dev/null
 
-tubeup http://twitch.tv/videos/$id "$rest" &
+tubeup http://twitch.tv/videos/$id $rest &
 (
 	set -o errexit
 	tcd --video $id --format all --settings-file ~/.config/tcd/"$config".json
