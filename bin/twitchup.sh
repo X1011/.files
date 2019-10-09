a98853c5c5ab187aa3da694f1c53ffd832f18c25
dan smith <XX1011@gmail.com>
2019-10-09 03:23:22 -0400

    twitchup.sh: wait for info file

--- bin/twitchup.sh
+++ bin/twitchup.sh
@@ -5,14 +5,22 @@ set -o errexit
 config=$1
 id=$2
 rest=${@:3}
+
+shopt -s nullglob
+find-info() { info=(*$id.info.json) ;}
+
 pushd ~/.tubeup/downloads/ >/dev/null
 
 tubeup http://twitch.tv/videos/$id $rest &
 (
 	set -o errexit
 	tcd --video $id --format all --settings-file ~/.config/tcd/"$config".json
-	info=(*$id.info.json)
+	
+	find-info
+	# if no file matches, wait and try again
+	[[ ${#info[*]} -gt 0 ]] || (sleep 5s; find-info)
 	base=${info%.info.json}
+	
 	echo # new line to reset cursor position from concurrent tubeup output
 	mv --verbose {$id,"$base"}.srt
 	mv --verbose {$id,"$base"}.chat.json
